7c121c1b5999105e590f8f35f971f62dd55bc88d
RonnieAlvarez <ronnie.alvarezc@gmail.com>
2023-12-13 22:05:55 -0600

    Add project files.

---
+++ 01_Clase_Console/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+Console.WriteLine("Digite el Nombre:");
+string txtNombre=Console.ReadLine();
+
+Console.WriteLine("Digite el Apellido:");
+string txtApellido = Console.ReadLine();
+
+Console.WriteLine("Digite el DNI:");
+string txtDNI = Console.ReadLine();
+
+Console.WriteLine("Digite la Edad:");
+string txtEdad = Console.ReadLine();
+
+Console.WriteLine("Digite el Altura:");
+string txtAltura = Console.ReadLine();
+
+Console.WriteLine($"Mi Nombre es {txtNombre} {txtApellido}, de {txtEdad} años, DNI: {txtDNI} y {txtAltura} de altura");
---
+++ Actividad1/Form1.cs
@@ -0,0 +1,62 @@
+namespace Actividad1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConcatenar_Click(object sender, EventArgs e)
+        {
+            lblResultado.Text = ($"Mi nombre es: {txtNombre.Text} {txtApellido.Text} con DNI {txtDni.Text}\n" +
+                $"tengo {txtEdad.Text} años y mido {txtAltura.Text} mts de altura.");
+        }
+
+        private void txtNombre_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                this.SelectNextControl((Control)sender, true, true, true, true);
+            }
+        }
+
+        private void txtApellido_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                this.SelectNextControl((Control)sender, true, true, true, true);
+            }
+        }
+
+        private void txtDni_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                this.SelectNextControl((Control)sender, true, true, true, true);
+            }
+        }
+
+        private void txtEdad_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                this.SelectNextControl((Control)sender, true, true, true, true);
+            }
+        }
+        private void txtAltura_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                btnConcatenar.Focus();
+            }
+        }
+
+       
+    }
+}
